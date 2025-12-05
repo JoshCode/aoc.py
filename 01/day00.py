3f69cd92da976b084b2bf173589bee49cba49d9b
JoshCode <joshcodedev@gmail.com>
2025-12-05 12:40:23 +0100

    Add template file

---
+++ 01/day00.py
@@ -0,0 +1,68 @@
+import io
+
+
+def part1(input: list[str]) -> int:
+    result: int = 0
+
+    return result
+
+
+def part2(input: list[str]) -> int:
+    result: int = 0
+
+    return result
+
+
+# region Input file handling
+def main():
+    # Define the following tuples to easily switch what runs
+    # First argument: If this part runs
+    # Second argument: Given True on first, if the puzzle input runs on this part
+    run_part1 = (True, False)
+    run_part2 = (True, False)
+
+    with (
+        open("./data/day04-example.txt", "r", encoding="utf-8") as example,
+        open("./data/day04-input.txt", "r", encoding="utf-8") as input,
+    ):
+        example_lines = []
+        for row in example.readlines():
+            example_lines.append(row.replace("\n", ""))
+
+        input_lines = []
+        for row in input.readlines():
+            input_lines.append(row.replace("\n", ""))
+
+        if run_part1[0]:
+            print("-> Running part 1")
+            print("   > Example")
+            out1_e = part1(example_lines)
+            if run_part1[1]:
+                print("   > Input")
+                out1_i = part1(input_lines)
+
+        if run_part2[0]:
+            example.seek(0, io.SEEK_SET)
+            input.seek(0, io.SEEK_SET)
+
+            print("-> Running part 2")
+            print("   > Example")
+            out2_e = part2(example_lines)
+            if run_part2[1]:
+                print("   > Input")
+                out2_i = part2(input_lines)
+
+        print("")
+        if run_part1[0]:
+            print(f"{out1_e=}")
+            if run_part1[1]:
+                print(f"{out1_i=}")
+        if run_part2[0]:
+            print(f"{out2_e=}")
+            if run_part2[1]:
+                print(f"{out2_i=}")
+
+
+if __name__ == "__main__":
+    main()
+# endregion
