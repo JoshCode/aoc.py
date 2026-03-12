28d77ca1f539ff9bed92ad68c15fea7dbc2c850e
JoshCode <joshcodedev@gmail.com>
2026-03-12 08:39:39 +0100

    Update 2015 day template

--- 2015/day00.py
+++ 2015/day00.py
@@ -18,18 +18,27 @@ def part2(input: list[str]) -> Any:
 
 # region Input file handling
 def main():
-    import io
     from pathlib import Path
     # Define the following tuples to easily switch what runs
     # First argument: If this part runs
     # Second argument: Given True on first, if the puzzle input runs on this part
     run_part1 = (True, False)
     run_part2 = (True, False)
-    input_path = Path(__file__).parent
+    
+    script_file = Path(__file__)
+    day_nr = script_file.stem[-2:]
+
+    script_path = script_file.parent
+
+    example_file = script_path / f"data/day{day_nr}-example.txt"
+    input_file = script_path / f"data/day{day_nr}-input.txt"
+
+    example_file.touch(exist_ok=True)
+    input_file.touch(exist_ok=True)
 
     with (
-        open(input_path / "data/day00-example.txt", "r", encoding="utf-8") as example,
-        open(input_path / "data/day00-input.txt", "r", encoding="utf-8") as input,
+        open(example_file, "r", encoding="utf-8") as example,
+        open(input_file, "r", encoding="utf-8") as input,
     ):
         example_lines: list[str] = []
         for row in example.readlines():
@@ -41,21 +50,20 @@ def main():
 
         out1_e = out1_i = out2_e = out2_i = None
 
-        if run_part1[0]:
+        if any(run_part1):
             print("-> Running part 1")
-            print("   > Example")
-            out1_e = part1(example_lines)
+            if run_part1[0]:
+                print("   > Example")
+                out1_e = part1(example_lines)
             if run_part1[1]:
                 print("   > Input")
                 out1_i = part1(input_lines)
 
-        if run_part2[0]:
-            example.seek(0, io.SEEK_SET)
-            input.seek(0, io.SEEK_SET)
-
+        if any(run_part2):
             print("-> Running part 2")
-            print("   > Example")
-            out2_e = part2(example_lines)
+            if run_part2[0]:
+                print("   > Example")
+                out2_e = part2(example_lines)
             if run_part2[1]:
                 print("   > Input")
                 out2_i = part2(input_lines)
@@ -63,12 +71,12 @@ def main():
         print("")
         if run_part1[0]:
             print(f"{out1_e=}")
-            if run_part1[1]:
-                print(f"{out1_i=}")
+        if run_part1[1]:
+            print(f"{out1_i=}")
         if run_part2[0]:
             print(f"{out2_e=}")
-            if run_part2[1]:
-                print(f"{out2_i=}")
+        if run_part2[1]:
+            print(f"{out2_i=}")
 
 
 if __name__ == "__main__":
