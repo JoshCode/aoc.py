db8f312e2fcef824f58c9a952059d08a6879fc94
JoshCode <joshcodedev@gmail.com>
2026-03-12 08:39:21 +0100

    Solve 2015 day 08

---
+++ 2015/day08.py
@@ -0,0 +1,95 @@
+from typing import Any
+
+def part1(input: list[str]) -> Any:
+    code_characters = 0
+    string_characters = 0
+
+    for line in input:
+        code_characters += len(line)
+        eval_line = eval(line)
+        if isinstance(eval_line, str):
+            string_characters += len(eval_line)
+        else:
+            raise ValueError(f"Invalid line: {line}")
+
+    return code_characters - string_characters
+
+
+def part2(input: list[str]) -> Any:
+    code_characters = 0
+    escaped_characters = 0
+
+    for line in input:
+        code_characters += len(line)
+        escaped = "\"" + line.replace('\\', '\\\\').replace('"', '\\\"') + "\""
+        escaped_characters += len(escaped)
+
+    return escaped_characters - code_characters
+
+
+# region Input file handling
+def main():
+    from pathlib import Path
+    # Define the following tuples to easily switch what runs
+    # First argument: If this part runs
+    # Second argument: Given True on first, if the puzzle input runs on this part
+    run_part1 = (True, True)
+    run_part2 = (True, True)
+    
+    script_file = Path(__file__)
+    day_nr = script_file.stem[-2:]
+
+    script_path = script_file.parent
+
+    example_file = script_path / f"data/day{day_nr}-example.txt"
+    input_file = script_path / f"data/day{day_nr}-input.txt"
+
+    example_file.touch(exist_ok=True)
+    input_file.touch(exist_ok=True)
+
+    with (
+        open(example_file, "r", encoding="utf-8") as example,
+        open(input_file, "r", encoding="utf-8") as input,
+    ):
+        example_lines: list[str] = []
+        for row in example.readlines():
+            example_lines.append(row.replace("\n", ""))
+
+        input_lines: list[str] = []
+        for row in input.readlines():
+            input_lines.append(row.replace("\n", ""))
+
+        out1_e = out1_i = out2_e = out2_i = None
+
+        if any(run_part1):
+            print("-> Running part 1")
+            if run_part1[0]:
+                print("   > Example")
+                out1_e = part1(example_lines)
+            if run_part1[1]:
+                print("   > Input")
+                out1_i = part1(input_lines)
+
+        if any(run_part2):
+            print("-> Running part 2")
+            if run_part2[0]:
+                print("   > Example")
+                out2_e = part2(example_lines)
+            if run_part2[1]:
+                print("   > Input")
+                out2_i = part2(input_lines)
+
+        print("")
+        if run_part1[0]:
+            print(f"{out1_e=}")
+        if run_part1[1]:
+            print(f"{out1_i=}")
+        if run_part2[0]:
+            print(f"{out2_e=}")
+        if run_part2[1]:
+            print(f"{out2_i=}")
+
+
+if __name__ == "__main__":
+    main()
+# endregion
